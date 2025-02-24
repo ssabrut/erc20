8c8eda58a2d88cd2f525e4f48aed1aef8e0a2fa7
Michael Eco <michael.gunawan2002@gmail.com>
2025-02-24 11:38:22 +0700

    feat: implement OurToken using ERC20 openzeppelin

--- src/OurToken.sol
+++ src/OurToken.sol
@@ -1,4 +1,10 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.19;
 
-contract OurToken {}
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract OurToken is ERC20 {
+    constructor(uint256 _initialSupply) ERC20("OurToken", "OT") {
+        _mint(msg.sender, _initialSupply);
+    }
+}
